8e415940d10e5e43475685697b740c6a2092ffd1
deepak <deepaksabu103@gmail.com>
2021-05-09 17:40:26 +0530

    first

---
+++ src/main/java/com/study/msgbrewery/MsgBreweryApplication.java
@@ -0,0 +1,13 @@
+package com.study.msgbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/study/msgbrewery/MsgBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.msgbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
